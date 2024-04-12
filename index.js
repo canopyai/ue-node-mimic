5aeedb862dfba7fe60102fc3b4eda3cfcb91967b
Amartya Varma <amartya.varma@gmail.com>
2024-04-12 18:22:49 +0100

    rm nm

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const wss = new WebSocket.Server({ port: 8080, host: '0.0.0.0' });
 
 const senderUrl = "ws://34.91.59.59:8080";
 
+// Open a connection to the server URL
+const senderWs = new WebSocket(senderUrl);
+
+senderWs.on('open', () => {
+    console.log('Connected to server URL:', senderUrl);
+});
+
+senderWs.on('message', (message) => {
+    console.log('Received message from server URL:', message);
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+        client.send(message);
+    });
+});
+
+senderWs.on('close', () => {
+    console.log('Disconnected from server URL:', senderUrl);
+});
+
 wss.on('connection', (ws) => {
     console.log('A client has connected.');
 
-    // Open a connection to the server URL
-    const senderWs = new WebSocket(senderUrl);
-
-    senderWs.on('open', () => {
-        console.log('Connected to server URL:', senderUrl);
-    });
-    
-    senderWs.on('message', (message) => {
-        console.log('Received message from server URL:', message);
-        ws.send(message);
-    });
-
-    senderWs.on('close', () => {
-        console.log('Disconnected from server URL:', senderUrl);
-    });
-
     ws.on('close', () => {
         console.log('A client has disconnected.');
-
-        // Close the connection to the server URL when the client disconnects
-        senderWs.close();
     });
 });
